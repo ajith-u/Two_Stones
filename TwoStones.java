421c5da60ff733edcc953f7be290f3d5a119adcd
ajith-u <69250281+ajith-u@users.noreply.github.com>
2021-01-11 17:53:03 +0530

    Add files via upload

---
+++ TwoStones.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class TwoStones{ 
+  public static void main(String args[]){  
+    System.out.println((new Scanner(System.in).nextInt()%2==0)?"Bob":"Alice");
+ 
+  }  
+} 
